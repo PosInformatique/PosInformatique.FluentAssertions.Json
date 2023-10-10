4ef8c626bdea0a459e5e33ae87893a6d33635d92
Gilles TOURREAU <gilles@tourreau.fr>
2023-10-10 22:51:16 +0200

    v1.0.0
    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
 # PosInformatique.FluentAssertions.Json
-PosInformatique.FluentAssertions.Json is a library to assert JSON serialization using the FluentAssertion style coding.
+PosInformatique.FluentAssertions.Json is a library to assert JSON serialization using the *Fluent Assertions* library style coding.
+
+## Installing from NuGet
+The [PosInformatique.FluentAssertions.Json](https://www.nuget.org/packages/PosInformatique.FluentAssertions.Json/)
+library is available directly on the
+[NuGet](https://www.nuget.org/packages/PosInformatique.FluentAssertions.Json/) official website.
+
+To download and install the library to your Visual Studio unit test projects use the following NuGet command line 
+
+```
+Install-Package PosInformatique.AspNet.WebForms.DependencyInjection
+```
+
+## How it is work?
+Imagine that you have the following JSON class:
+
+```csharp
+public class Customer
+{
+    [JsonPropertyName("id")]
+    [JsonPropertyOrder(1)]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    [JsonPropertyOrder(2)]
+    public string Name { get; set; }
+}
+```
+
+With the following instance:
+
+```csharp
+var customer = new Customer()
+{
+    Id = 1234,
+    Name = "Gilles TOURREAU",
+};
+```
+
+You would like to check this class is serializable into the following JSON object:
+
+```json
+{
+    "id": 1234,
+    "name": "Gilles TOURREAU",
+}
+```
+
+Using standard assertions you should write the following code :fearful::
+```
+[Fact]
+public void Serialization()
+{
+    var customer = new Customer()
+    {
+        Id = 1234,
+        Name = "Gilles TOURREAU",
+    };
+
+    var json = JsonSerializer.Serialize(customer);
+
+    json.Should().Be("{\"id\":1234,\"name\":\"Gilles TOURREAU\"}");
+    
+    // Or
+    json.Should().Be(@"{""id"":1234,""name"":""Gilles TOURREAU""}");
+}
+```
+
+With the following kind of exception when the unit test is incorrect:
+![Ugly exception](./docs/UglyExceptionExample.png)
+
+As you can see the previous code is not sexy to read (and to write!) and the exception is
+hard to understand...
+
+## Test the serialization of a .NET Object to a JSON object.
+With the new fluent style using this library you can write previous unit test like that:
+
+```csharp
+[Fact]
+public void Serialization()
+{
+    var customer = new Customer()
+    {
+        Id = 1234,
+        Name = "Gilles TOURREAU",
+    };
+
+    customer.Should().BeJsonSerializableInto(new
+    {
+        id = 1234,
+        name = "Gilles TOURREAU",
+    });
+}
+```
+
+And when an exception is occured, the exception message contains the JSON path of the property which is error:
+![Pretty exception](./docs/PrettyExceptionExample.png)
+
+### Test the deserialization of a JSON object to a .NET Object
+You can in the same way test the deserialization JSON object into a .NET object.
+
+```
+[Fact]
+public void Deserialization()
+{
+    var json = new
+    {
+        id = 1234,
+        name = "Gilles TOURREAU",
+    };
+
+    json.Should().BeJsonDeserializableInto(new Customer()
+    {
+        Id = 1234,
+        Name = "Gilles TOURREAU",
+    });
+}
+```
---
+++ src/FluentAssertions.Json/JsonFluentAssertionsExtensions.cs
@@ -0,0 +1,190 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="JsonFluentAssertionsExtensions.cs" company="P.O.S Informatique">
+//     Copyright (c) P.O.S Informatique. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace FluentAssertions
+{
+    using System.Diagnostics;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+    using FluentAssertions.Common;
+    using FluentAssertions.Primitives;
+
+    /// <summary>
+    /// Contains extension methods to check if an object is serializable or deserializable to JSON object.
+    /// </summary>
+    [DebuggerNonUserCode]
+    public static class JsonFluentAssertionsExtensions
+    {
+        private static readonly JsonSerializerOptions JsonSerializationOptions = new JsonSerializerOptions()
+        {
+            Converters =
+            {
+                new JsonStringEnumConverter(),
+            },
+        };
+
+        /// <summary>
+        /// Check if the subject object in <paramref name="assertions"/> instance is serializable into the JSON object
+        /// specified by the <paramref name="expectedJson"/>.
+        /// </summary>
+        /// <param name="assertions"><see cref="ObjectAssertions"/> which contains the object subject to check.</param>
+        /// <param name="expectedJson">The object which represents the raw JSON object expected.</param>
+        public static void BeJsonSerializableInto(this ObjectAssertions assertions, object? expectedJson)
+        {
+            var jsonString = JsonSerializer.Serialize(assertions.Subject, JsonSerializationOptions);
+
+            var deserializedJsonDocument = JsonSerializer.Deserialize<JsonDocument>(jsonString, JsonSerializationOptions);
+
+            if (deserializedJsonDocument is null)
+            {
+                if (expectedJson != null)
+                {
+                    Services.ThrowException("A JSON object was expected.");
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            var expectedJsonDocument = JsonSerializer.SerializeToDocument(expectedJson, JsonSerializationOptions);
+
+            Compare(deserializedJsonDocument!, expectedJsonDocument);
+        }
+
+        /// <summary>
+        /// Check if the JSON subject object is deserializable into the specified <paramref name="expectedObject"/> argument.
+        /// </summary>
+        /// <typeparam name="T">Type of the object to deserialize from JSON.</typeparam>
+        /// <param name="assertions"><see cref="ObjectAssertions"/> which contains the JSON object subject to deserialize.</param>
+        /// <param name="expectedObject">Expected object deserialized expected.</param>
+        public static void BeJsonDeserializableInto<T>(this ObjectAssertions assertions, T expectedObject)
+        {
+            var jsonText = JsonSerializer.Serialize(assertions.Subject, JsonSerializationOptions);
+            var deserializedObject = JsonSerializer.Deserialize<T>(jsonText, JsonSerializationOptions);
+
+            deserializedObject.Should().BeEquivalentTo(expectedObject);
+        }
+
+        private static void Compare(JsonDocument document, JsonDocument expected)
+        {
+            var path = new Stack<string>();
+
+            path.Push("$");
+
+            Compare(document.RootElement, expected.RootElement, path);
+
+            path.Pop();
+        }
+
+        private static void Compare(JsonElement element, JsonElement expected, Stack<string> path)
+        {
+            if (element.ValueKind != expected.ValueKind)
+            {
+                Services.ThrowException($"{GetPath(path)}: Expected property to be '{expected.ValueKind}' type instead of '{element.ValueKind}' type.");
+            }
+            else if (element.ValueKind == JsonValueKind.String)
+            {
+                var value = element.GetString();
+                var expectedValue = expected.GetString();
+
+                if (value != expectedValue)
+                {
+                    Services.ThrowException($"{GetPath(path)}: Expected '{expectedValue}' instead of '{value}'.");
+                }
+            }
+            else if (element.ValueKind == JsonValueKind.Number)
+            {
+                var value = element.GetDouble();
+                var expectedValue = expected.GetDouble();
+
+                if (value != expectedValue)
+                {
+                    Services.ThrowException($"{GetPath(path)}: Expected '{expectedValue}' instead of '{value}'.");
+                }
+            }
+            else if (element.ValueKind == JsonValueKind.Object)
+            {
+                var expectedPropertyEnumerator = expected.EnumerateObject();
+
+                foreach (var property in element.EnumerateObject())
+                {
+                    if (!expectedPropertyEnumerator.MoveNext())
+                    {
+                        Services.ThrowException($"{GetPath(path)}: Expected no property but found '{property.Name}' property.");
+                    }
+
+                    var expectedProperty = expectedPropertyEnumerator.Current;
+
+                    if (property.Name != expectedProperty.Name)
+                    {
+                        Services.ThrowException($"{GetPath(path)}: Expected property with the '{expectedProperty.Name}' name but found '{property.Name}' instead.");
+                    }
+
+                    path.Push("." + property.Name);
+
+                    Compare(property.Value, expectedProperty.Value, path);
+
+                    path.Pop();
+                }
+
+                if (expectedPropertyEnumerator.MoveNext())
+                {
+                    Services.ThrowException($"{GetPath(path)}: Expected '{expectedPropertyEnumerator.Current.Name}' property but found no property.");
+                }
+            }
+            else if (element.ValueKind == JsonValueKind.Array)
+            {
+                var expectedArrayEnumerator = expected.EnumerateArray();
+                var index = 0;
+
+                foreach (var item in element.EnumerateArray())
+                {
+                    if (!expectedArrayEnumerator.MoveNext())
+                    {
+                        var actualCount = element.EnumerateArray().Count();
+
+                        Services.ThrowException($"{GetPath(path)}: Expected {index} item(s) but found {actualCount}.");
+                    }
+
+                    var expectedItem = expectedArrayEnumerator.Current;
+
+                    path.Push($"[{index}]");
+
+                    Compare(item, expectedItem, path);
+
+                    path.Pop();
+
+                    index++;
+                }
+
+                if (expectedArrayEnumerator.MoveNext())
+                {
+                    var expectedCount = expected.EnumerateArray().Count();
+
+                    Services.ThrowException($"{GetPath(path)}: Expected {expectedCount} item(s) but found {index}.");
+                }
+            }
+            else if (element.ValueKind == JsonValueKind.True)
+            {
+                return;
+            }
+            else if (element.ValueKind == JsonValueKind.False)
+            {
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private static string GetPath(IEnumerable<string> path)
+        {
+            return string.Concat(path.Reverse());
+        }
+    }
+}
